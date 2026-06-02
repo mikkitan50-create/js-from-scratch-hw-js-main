e306f8dbbddf9265169990a8a5464de0526da8d7
Miha Turaev <mikkitan50@gmail.com>
2026-06-02 19:42:13 +0300

    Второе задание 6 урока, готово

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,4 +24,20 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+    const result = []
+    
+    for (let i = 0; i < array.length; i++) {
+        result.push(callback(array[i], i))
+    }
+    
+    return result
+}
+
+const numbers = [1, 2, 3, 4, 5]
+
+const doubledNumbers = map(numbers, (element, index) => {
+    return element * 2
+})
+
+console.log(doubledNumbers)
